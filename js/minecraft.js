d7388461e91139684062f91208f6b4bd95e6a9c7
KyleTheUndead <kyletheundead666@gmail.com>
2022-06-30 17:12:54 +0200

    text update

--- js/minecraft.js
+++ js/minecraft.js
@@ -4,11 +4,11 @@ $.getJSON(url, function(result) {
     //data is the JSON string
 if(result.players.max == 0){
     $('#mc-status').html('Offline');
-    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' ⁃ '));
+    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' - '));
     $('#mc-players').html("/");
 }else{
     $('#mc-status').html('Online');
-    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' ⁃ '));
+    $('#mc-version').html(result.version.name.replace('Velocity ', '').replace('-', ' - '));
     $('#mc-players').html(result.players.online + "/" + result.players.max);
 }
 });
